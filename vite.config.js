87b069c8c75b266f30d56247d1f1e37e565f8bd7
tenseiof <edotenseiof@gmail.com>
2025-04-23 21:04:53 +0300

    env

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+const viteEnv = {};
+Object.keys(process.env).forEach(key => {
+	if (key.startsWith('VITE_')) {
+		viteEnv[`import.meta.env.${key}`] = process.env[key];
+	}
+});
 
-// https://vite.dev/config/
+// https://vite.dev/config
 export default defineConfig({
-	base: '/',
+	define: viteEnv,
 	plugins: [react(), tailwindcss()]
 });
